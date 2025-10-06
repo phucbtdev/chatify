37621ec4f1d5d721ab32e14e8298154f1d52d18e
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-10-06 21:59:34 +0700

    fix bug

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -1,10 +1,10 @@
-import Message from '../model/message.model.js';
+import User from '../model/user.model.js';
 
 export const getAllContacts = async (req, res) => {
     try {
 
         const loggedInUserId = req.user._id;
-        const filteredUsers = await Message.find({ _id: { $ne: loggedInUserId } }).select("-password");
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
         res.status(200).json(filteredUsers);
     } catch (error) {
