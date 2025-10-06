c9618aadcd21a184004f1a83872e3b65fee423a0
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-10-06 22:03:12 +0700

    created getMessagesByUserId method

--- backend/src/controller/message.controller.js
+++ backend/src/controller/message.controller.js
@@ -14,7 +14,20 @@ export const getAllContacts = async (req, res) => {
 };
 
 export const getMessagesByUserId = async (req, res) => {
-
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { sender: myId, receiver: userToChatId },
+                { sender: userToChatId, receiver: myId }
+            ]
+        })
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error fetching messages:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const sendMessage = async (req, res) => {
